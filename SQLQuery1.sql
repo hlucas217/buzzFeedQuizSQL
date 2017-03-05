2aee2398e5154bc316ef6cddb87b6b07c24ddab8
Heather Lucas <hlucas@gmail.com>
2017-03-04 21:15:51 -0500

    first commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿--SELECT COUNT (*) as countie FROM Questions 
+--SELECT Question, Answer FROM Questions JOIN Answers ON Questions.QuestionId = answers.QuestionId WHERE Questions.QuestionId
+--SELECT * FROM Questions
+--SELECT * FROM Answers
+--SELECT * FROM Questions JOIN Answers ON Questions.QuestionId = answers.QuestionId WHERE questions.testId = questions.testId
+--SELECT FROM question JOIN test ON questions.question = tests.testid WHERE questions.testID = 3
+--SELECT COUNT (*) as answers from answers WHERE Questions.TestId = answers.QuestionId
+SELECT Answer, Question From answers JOIN questions ON Questions.QuestionId = Answers.QuestionId WHERE TestId = 3
+SELECT COUNT (*) as countie (SELECT answer, question FROM questions JOIN answers ON questions.QuestionId = answers.QuestionId WHERE TestId = 3)
